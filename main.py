c1695b8bafdb63d891174e668be1478059470572
Matthew Morales <mtthwmrls9@gmail.com>
2025-09-28 12:37:21 -0600

    Write data import

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import torch
-x = torch.rand(5, 3)
-print(x)
+import torch as tch
+import torchvision as tv
+
+train_data = tv.datasets.EMNIST(
+    root="./data",
+    split="letters",
+    train=True,
+    download=True
+)
+
+print(train_data[0])
